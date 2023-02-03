c89117947762d7a7126a8dc63daf033b5aa8149f
Stepan Friedl <friedl@cesnet.cz>
2023-02-03 14:44:18 +0100

    CARD: [FEATURE] F-Tile IP - Add 100GE-2 (NRZ) IP

--- src/Modules.tcl
+++ src/Modules.tcl
@@ -35,6 +35,7 @@ if {$ARCHGRP_ARR(NET_MOD_ARCH) == "F_TILE"} {
     }
     if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 100} {
         set MOD "$MOD $ENTITY_BASE/ip/ftile_pll_4x100g.ip"
+        set MOD "$MOD $ENTITY_BASE/ip/ftile_eth_2x100g.ip"
         set MOD "$MOD $ENTITY_BASE/ip/ftile_eth_4x100g.ip"
     }
     if {$ARCHGRP_ARR(ETH_PORT_SPEED,0) == 50} {
